3ff524c61b5c74240a4d58e63e85492ba388e6e3
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-02-09 16:26:01 +0100

    cannot sign transaction with this key error

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 const bitcoin = require('bitcoinjs-lib');
 const crypto = require('crypto');
 const ecc = require('tiny-secp256k1')
-const {ECPairFactory} = require('ecpair')
+const {ECPairFactory} = require('ecpair');
+const { regtest, testnet } = require('bitcoinjs-lib/src/networks');
 
 
 const generateRedeemScript = (str) => {
@@ -15,10 +16,10 @@ const generateRedeemScript = (str) => {
   // generate the redeem script
   const redeemScript = bitcoin.script.compile([
     bitcoin.opcodes.OP_SHA256,
-    hash,
+    "427472757374204275696c64657273", // use the hex instead
     bitcoin.opcodes.OP_EQUAL
   ])
-
+  // console.log(`hash: ${hash.toString('hex')}, redeemScript: ${redeemScript.toString('hex')}`)
   return redeemScript;
 }
 
@@ -33,9 +34,24 @@ const generateRedeemScriptHex = (str) => {
   // // Compute the hash of the redeem script
   // const redeemScriptHash = bitcoin.crypto.hash160(redeemScript);
   // console.log(`Redeem Script (Hash): ${redeemScriptHash}`)
+  return redeemScriptHex;
 }
 
+const testnetAddress = () => {
+  const ECPair = ECPairFactory(ecc)
+  const keyPair = ECPair.makeRandom({network: bitcoin.networks.testnet})
+  const privateKey = keyPair.privateKey; // Private key in WIF format
+  const inputIndex = 0;
+
+  const {address} = bitcoin.payments.p2pkh({pubkey: keyPair.publicKey, network: bitcoin.networks.testnet});
+  // console.log(`p2pkh address: ${address}`)
+}
+// testnetAddress()
+
 generateRedeemScriptHex("Btrust Builders");
+const lock_hex = "427472757374204275696c64657273"
+console.log(`OP_SHA256 ${lock_hex} OP_EQUAL`)
+const redeemScript = bitcoin.script.fromASM(`OP_SHA256 ${lock_hex} OP_EQUAL`)
 
 const getAddress = (str) => {
   const redeemScript = generateRedeemScript(str);
@@ -50,7 +66,7 @@ const getAddress = (str) => {
    return address;
 }
 
-console.log(getAddress("Btrust Builders"));
+// console.log(getAddress("Btrust Builders"));
 
 // Create a new transaction builder
 // const txb = new bitcoin.TransactionBuilder(bitcoin.networks.bitcoin);
@@ -74,8 +90,9 @@ async function getTxn(txid) {
   return data
 }
 
-const txid = 'c09fee606fa5209d7cb0aac9f7628f6c413d0e2d330496077de0dc393e4220f2'
-const nonWitnessUtxo = getTxn(txid)
+const testnet_txid = '328f481eb1f8c599164c65f4931bb2d002eb03b8084958524b516a6288569d3d'
+// const txid = 'd5940f905341eadd698f9d3dda9087270a9138e85da2c6d0496fe0bd7e73ba65' // local version
+const nonWitnessUtxo = getTxn(testnet_txid)
 .then(rawTx => {
    const result = Buffer.from(rawTx, 'hex')
   //  console.log(result) // print the nonWitnessUtxo value
@@ -89,14 +106,14 @@ console.log(nonWitnessUtxo)
 const sendBtcToAddress = async () => {
   const result = await nonWitnessUtxo
   const outputNumber = 0;
-  const amount = 0.00086528;
+  const amount = 0.00064161;
   // const destinationAddress = 'mv4rnyY3Su5gjcDNzbMLKBQkBicCtHUtFB';
   const destinationAddress = getAddress("Btrust Builders")
   const minerFee = 10000;
   const outputAmount = amount*1e8 - minerFee;
 
   const utxo = {
-    hash: txid,
+    hash: testnet_txid,
     index: outputNumber,
     nonWitnessUtxo: Buffer.from(result, 'hex'), // replace 'nonWitnessUtxo' with actual value
   };
@@ -108,12 +125,27 @@ const sendBtcToAddress = async () => {
   // Sign the transaction
   // Replace with actual private key and input index
   const ECPair = ECPairFactory(ecc)
-  const keyPair = ECPair.makeRandom()
-  const privateKey = keyPair.privateKey; // Private key in WIF format
+  const keyPair = ECPair.makeRandom({network: testnet})
+  const privateKey = keyPair.privateKey; 
   const inputIndex = 0;
-  // const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: bitcoin.ECPair.fromPrivateKey(privateKey).publicKey });
-  const p2sh = bitcoin.payments.p2sh({ redeem: {output: generateRedeemScript("Btrust Builders")}, network: bitcoin.networks.regtest});
-  // const signature = txb.signInput(0, keyPair);
+
+  // const {address} = bitcoin.payments.p2sh({pubkey: keyPair.publicKey, network: bitcoin.networks.testnet});
+  // console.log(`p2pkh address: ${address}`)
+
+
+  // const lock_hex = "427472757374204275696c64657273"
+  // // console.log(`OP_SHA256 ${lock_hex} OP_EQUAL`)
+  // const redeemScript = bitcoin.script.fromASM(`OP_SHA256 ${lock_hex} OP_EQUAL`)
+
+  // // const scriptPubKey = bitcoin.payments.p2sh({ redeem: {output: redeemScript}, network: bitcoin.networks.regtest});
+
+  // // const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: bitcoin.ECPair.fromPrivateKey(privateKey).publicKey });
+  // const scriptPubKey = bitcoin.payments.p2sh({ redeem: {output: generateRedeemScript("Btrust Builders")}, network: bitcoin.networks.regtest});
+
+  // // console.log(scriptPubKey)
+  const signature = txb.signInput(0, keyPair);
+
+  
   // txb.finalizeAllInputs()
   
   // // Get the raw transaction in hex format
